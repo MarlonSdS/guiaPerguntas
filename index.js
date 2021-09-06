14e336def3f8f5c3ccfde8e6bdda755c08bdd816
Marlon Santana dos Santos <marlonfms2012@gmail.com>
2021-09-06 10:32:07 -0300

    começando os formulários

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+//const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));
+
+app.use(express.urlencoded({extended: true}));
+//app.use(express.json);
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -12,6 +16,11 @@ app.get("/perguntar", (req, res) =>{
     res.render("perguntar");
 })
 
-app.listen(8080, ()=>{
+app.post("/salvarpergunta", (req, res) =>{
+    var titulo = req.body.titulo;
+    res.send("Formulário recebido!"+titulo);
+})
+
+app.listen(2525, ()=>{
     console.log("The server is running...");
 })
